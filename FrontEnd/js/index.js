23bfc86698c6cca7c586dcb2999a1a5d0c3f4c5e
DanielDSCosta <131755890+DanielDSCosta@users.noreply.github.com>
2023-07-20 11:35:55 +0200

    correction

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -10,29 +10,29 @@ async function fetchWorks() {
 }
 fetchWorks();
 
-/* Function pour créer la gallerie */
+/* Function pour créer la galerie */
 function createGallery(objet) {
   let galleries = "";
   for (let work of objet) {
     galleries += `
       <figure>
-          <img src="${work.imageUrl}">
-          <figcaption>${work.title}</figcaption>
+        <img src="${work.imageUrl}">
+        <figcaption>${work.title}</figcaption>
       </figure>`;
   }
   gallery.innerHTML = galleries;
 }
 
-/* Function pour créer les boutons */
+/* Function pour créer les boutons de filtre */
 function createButtonFilters() {
   document.querySelector(".filtres").innerHTML = `
-  <button class="buttonTous">Tous</button>
-  <button class="buttonObjets">Objets</button>
-  <button class="buttonAppartements">Appartements</button>
-  <button class="buttonHotelRestaurants">Hôtels & restaurants</button>`;
+    <button class="buttonTous">Tous</button>
+    <button class="buttonObjets">Objets</button>
+    <button class="buttonAppartements">Appartements</button>
+    <button class="buttonHotelRestaurants">Hôtels & restaurants</button>`;
 }
 
-/* Initialisation de la variable CATEGORIES */
+/* Initialisation des catégories */
 const CATEGORIES = {
   reset: 0,
   objects: 1,
@@ -40,16 +40,16 @@ const CATEGORIES = {
   hostel: 3,
 };
 
-/* Gestion du changement de categories */
+/* Gestion du changement de catégories */
 const handleFilterWorks = (categoryId = CATEGORIES.objects) => {
   if (categoryId === CATEGORIES.reset) return works;
 
-  return works.filter((event) => {
-    return event.category.id === categoryId;
+  return works.filter((work) => {
+    return work.category.id === categoryId;
   });
 };
 
-/* Gestion du Filled au click de la categorie */
+/* Gestion du style au clic de la catégorie */
 const toggleActiveButtonStyles = (element) => {
   Array.from(document.querySelector(".filtres").children).forEach(
     (filterElement) => {
@@ -62,29 +62,25 @@ const toggleActiveButtonStyles = (element) => {
   );
 };
 
-/* Lancement des functions */
-fetchWorks(works);
+/* Lancement des fonctions */
 createGallery(works);
 createButtonFilters();
 
-/* Evenement on click celon les boutons */
+/* Événements au clic des boutons */
 document.querySelector(".buttonTous").addEventListener("click", () => {
   const workFiltered = handleFilterWorks(CATEGORIES.reset);
-
   createGallery(workFiltered);
   toggleActiveButtonStyles(document.querySelector(".buttonTous"));
 });
 
 document.querySelector(".buttonObjets").addEventListener("click", () => {
   const workFiltered = handleFilterWorks(CATEGORIES.objects);
-
   createGallery(workFiltered);
   toggleActiveButtonStyles(document.querySelector(".buttonObjets"));
 });
 
 document.querySelector(".buttonAppartements").addEventListener("click", () => {
   const workFiltered = handleFilterWorks(CATEGORIES.flat);
-
   createGallery(workFiltered);
   toggleActiveButtonStyles(document.querySelector(".buttonAppartements"));
 });
@@ -93,62 +89,54 @@ document
   .querySelector(".buttonHotelRestaurants")
   .addEventListener("click", () => {
     const workFiltered = handleFilterWorks(CATEGORIES.hostel);
-
     createGallery(workFiltered);
     toggleActiveButtonStyles(document.querySelector(".buttonHotelRestaurants"));
   });
 
 /* LOGIN et Modale */
 
-/* Recupère les données dans le storage */
+const logoutElement = document.getElementById("logout");
 const token = localStorage.getItem("token");
+const isLoggedIn = !!token;
 
-// Je check si le token est dans le localstorage si oui je montre la bannière et les boutons modifier
-// sinon je montre les boutons pour filtrer les travaux
-const userLoginCheck = () => {
-  document.addEventListener("DOMContentLoaded", () => {
-    const token = localStorage.getItem("token");
-    if (token != null) {
-      changeLoginToLogout();
-    } else {
-      createButtonFilters();
-    }
-  });
-};
-userLoginCheck();
+const loggedInBannerElement = document.querySelector("#loggedin-banner");
+const loggedInModifElement = document.querySelectorAll(".loggedin-modif");
 
-// Je change le nom login vers logout en mode edition
-const changeLoginToLogout = () => {
-  const logoutElement = document.getElementById("logout");
-  logoutElement.textContent = "logout";
-  logoutElement.addEventListener("click", () => {
-    localStorage.clear("token");
-  });
-};
+loggedInBannerElement.style.display = isLoggedIn ? "flex" : "none";
+logoutElement.textContent = isLoggedIn ? "logout" : "login";
 
-// Récupérer les éléments de la modale
-const modale = document.querySelector("modale");
-const openModaleBtn = document.querySelector("fa-regular fa-pen-to-square");
-const closeModaleBtn = document.querySelector(".closeModale");
-const selfDestructBtn = document.querySelector("selfDestructBtn");
+logoutElement.addEventListener("click", () => {
+  localStorage.clear("token");
+});
+
+console.log(logoutElement);
+console.log(token);
+console.log(isLoggedIn);
+console.log(loggedInBannerElement);
+console.log(loggedInModifElement);
+
+// Récupérer les éléments de la modal
+const modalGalleryElement = document.querySelector("#modal-gallery");
+const openGalleryModalButtonElement = document.querySelector(
+  "#open-gallery-modal-button"
+);
+const closeGalleryModalButtonElement = document.querySelector(
+  "#close-gallery-modal-button"
+);
+
+const closeGalleryModalIconElement = document.querySelector(
+  "#close-gallery-modal-icon"
+);
 const arrowLeftBtn = document.querySelector(".arrowLeft");
 const modale2 = document.querySelector(".modale2");
 
-// Fonction pour ouvrir la modale
-function openModale() {
-  modale.style.display = "block";
-}
+openGalleryModalButtonElement.addEventListener("click", () => {
+  modalGalleryElement.style.display = "flex";
+});
 
-// Fonction pour fermer la modale
-function closeModale() {
-  modale.style.display = "none";
-}
-
-// Gérer l'événement clic sur le bouton pour ouvrir la modale
-openModaleBtn.addEventListener("click", openModale);
-
-// Gérer l'événement clic sur le bouton pour fermer la modale
-closeModaleBtn.addEventListener("click", closeModale);
+closeGalleryModalButtonElement.addEventListener("click", () => {
+  modalGalleryElement.style.display = "none";
+});
 
 // Gérer l'événement clic sur le bouton pour supprimer la galerie
 selfDestructBtn.addEventListener("click", function (event) {
@@ -156,36 +144,41 @@ selfDestructBtn.addEventListener("click", function (event) {
   if (confirm("Voulez-vous vraiment supprimer la galerie?")) {
     // Code pour supprimer la galerie
     console.log("Galerie supprimée !");
-    closeModale(); // Fermer la modale après la suppression
+    closeModale(); // Fermer la modal après la suppression
   }
 });
 
-// Gérer l'événement clic sur le bouton pour revenir à la modale principale depuis modale2
+// Gérer l'événement clic sur le bouton pour revenir à la modal principale depuis modale2
 arrowLeftBtn.addEventListener("click", function (event) {
   event.preventDefault(); // Empêcher le comportement de lien par défaut
   modale2.style.display = "none"; // Cacher modale2
 });
 
 // Gérer l'événement clic sur le bouton "Ajouter une photo" pour afficher modale2
-openModaleBtn.addEventListener("click", function (event) {
+openGalleryModalButtonElement.addEventListener("click", function (event) {
   event.preventDefault(); // Empêcher le comportement de lien par défaut
   modale2.style.display = "block"; // Afficher modale2
 });
 
-// Fonction pour afficher ou masquer les boutons "modifier" en fonction de l'état de connexion
-function updateButtonVisibility() {
-  const isLoggedInUser = userLoginCheck(); // Vérifiez l'état de connexion en utilisant la fonction définie ci-dessus.
-  const editButtons = document.querySelectorAll(
-    ".fa fa-light fa-pen-to-square"
-  );
+/*const editLinks = document.querySelectorAll(".link-modal");
 
-  // Parcours de tous les boutons "modifier"
-  editButtons.forEach((a) => {
+const updateButtonVisibility = () => {
+  const isLoggedInUser = isLoggedIn; // Utilisez la variable isLoggedIn pour vérifier l'état de connexion
+  const logoutElement = document.getElementById("logout");
+  logoutElement.textContent = "logout";
+  logoutElement.addEventListener("click", () => {
+    localStorage.clear("token");
+  });
+
+  // Utilisez editLinks au lieu de editButtons
+  editLinks.forEach((link) => {
     // Affiche le bouton si l'utilisateur est connecté
     // ou masque le bouton si l'utilisateur n'est pas connecté
-    button.style.display = isLoggedInUser ? "block" : "none";
+    link.style.display = isLoggedInUser ? "block" : "none";
   });
-}
+};
 
 // Appelez la fonction pour mettre à jour la visibilité des boutons au chargement de la page.
-document.addEventListener("DOMContentLoaded", updateButtonVisibility);
+document.addEventListener("DOMContentLoaded", () => {
+  updateButtonVisibility();
+});*/
